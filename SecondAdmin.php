d6eb16a96a50f011956068f3a1f5ace63cdc0134
Godwin KPOGLO <kpoglo.godwin10@gmail.com>
2022-08-01 22:26:16 +0000

    hii

--- SecondAdmin.php
+++ SecondAdmin.php
@@ -1,4 +1,4 @@
-<? php
+<?php
 session_start();
 include("config.php");
 if ($_SESSION["connecter"] != "yes") {
@@ -13,10 +13,8 @@ $bienvenue = //"Bonsoir et bienvenue "  .
 $_SESSION["bin"];
 ?>
 
-
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
 
     <meta charset="utf-8">
@@ -25,24 +23,21 @@ $_SESSION["bin"];
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>SB Admin 2 - Tables</title>
+    <title>SB Admin 2 - Dashboard</title>
 
-    <!-- Custom fonts for this template -->
+    <!-- Custom fonts for this template-->
     <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
 
-    <!-- Custom styles for this template -->
+    <!-- Custom styles for this template-->
     <link href="css/sb-admin-2.min.css" rel="stylesheet">
-
-    <!-- Custom styles for this page -->
-    <link href="vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
 
 </head>
 
-<body id="page-top">
-
+<body onLoad="document.fo.login.focus()" id="page-top">
+<!--a  href="deconnexion.php">Se déconnecter</a-->
     <!-- Page Wrapper -->
     <div id="wrapper">
 
@@ -52,7 +47,7 @@ $_SESSION["bin"];
             <!-- Sidebar - Brand -->
             <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
                 <div class="sidebar-brand-icon rotate-n-15">
-                    <!--i class="fas fa-laugh-wink"></i-->
+                    
                 </div>
                 <div class="sidebar-brand-text mx-3">OPTIONS <sup></sup></div>
             </a>
@@ -61,7 +56,7 @@ $_SESSION["bin"];
             <hr class="sidebar-divider my-0">
 
             <!-- Nav Item - Dashboard -->
-            <li class="nav-item">
+            <li class="nav-item active">
                 <a class="nav-link" href="SuperAdmin.php">
                     <i class="fas fa-fw fa-tachometer-alt"></i>
                     <span>Page d'accueil</span></a>
@@ -72,11 +67,11 @@ $_SESSION["bin"];
 
             <!-- Heading -->
             <div class="sidebar-heading">
-                Interface
+                Options
             </div>
 
             <!-- Nav Item - Pages Collapse Menu -->
-            <li class="nav-item">
+            <!--li class="nav-item">
                 <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
                     aria-expanded="true" aria-controls="collapseTwo">
                     <i class="fas fa-fw fa-cog"></i>
@@ -84,12 +79,12 @@ $_SESSION["bin"];
                 </a>
                 <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
                     <div class="bg-white py-2 collapse-inner rounded">
-                        <h6 class="collapse-header">Options:</h6>
-                        <a class="collapse-item" href="AjouterAdmin.php">Ajouter</a>
-                        <a class="collapse-item" href="cards.html">Cards</a>
-                    </div>
+                        <h6 class="collapse-header"></h6>
+                        <a class="collapse-item" href="AjouterAdmin.php">Ajouter</a-->
+                        <!--a class="collapse-item" href="cards.html">Cards</a-->
+                    <!--/div>
                 </div>
-            </li>
+            </li-->
 
             <!-- Nav Item - Utilities Collapse Menu -->
             <li class="nav-item">
@@ -138,11 +133,11 @@ $_SESSION["bin"];
             </li>
 
             <!-- Nav Item - Tables -->
-            <li class="nav-item active">
+            <!--li class="nav-item">
                 <a class="nav-link" href="Adminaff.php">
                     <i class="fas fa-fw fa-table"></i>
-                    <span>Afficher les administrateurs</span></a>
-            </li>
+                    <span>Afficher les adminstrateurs</span></a>
+            </li-->
 
             <!-- Divider -->
             <hr class="sidebar-divider d-none d-md-block">
@@ -151,6 +146,9 @@ $_SESSION["bin"];
             <div class="text-center d-none d-md-inline">
                 <button class="rounded-circle border-0" id="sidebarToggle"></button>
             </div>
+
+            <!-- Sidebar Message -->
+            
 
         </ul>
         <!-- End of Sidebar -->
@@ -165,11 +163,9 @@ $_SESSION["bin"];
                 <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
 
                     <!-- Sidebar Toggle (Topbar) -->
-                    <form class="form-inline">
-                        <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
-                            <i class="fa fa-bars"></i>
-                        </button>
-                    </form>
+                    <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
+                        <i class="fa fa-bars"></i>
+                    </button>
 
                     <!-- Topbar Search -->
                     <!--form
@@ -189,13 +185,13 @@ $_SESSION["bin"];
                     <ul class="navbar-nav ml-auto">
 
                         <!-- Nav Item - Search Dropdown (Visible Only XS) -->
-                        <!--li class="nav-item dropdown no-arrow d-sm-none">
+                        <li class="nav-item dropdown no-arrow d-sm-none">
                             <a class="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button"
                                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                 <i class="fas fa-search fa-fw"></i>
-                            </a-->
+                            </a>
                             <!-- Dropdown - Messages -->
-                            <!--div class="dropdown-menu dropdown-menu-right p-3 shadow animated--grow-in"
+                            <div class="dropdown-menu dropdown-menu-right p-3 shadow animated--grow-in"
                                 aria-labelledby="searchDropdown">
                                 <form class="form-inline mr-auto w-100 navbar-search">
                                     <div class="input-group">
@@ -210,7 +206,10 @@ $_SESSION["bin"];
                                     </div>
                                 </form>
                             </div>
-                        </li-->
+                        </li>
+
+                       
+                        
 
                         <div class="topbar-divider d-none d-sm-block"></div>
 
@@ -219,12 +218,14 @@ $_SESSION["bin"];
                             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
                                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                 <span class="mr-2 d-none d-lg-inline text-gray-600 small"><?php if(isset($bienvenue)){
-                                	echo $bienvenue;}  ?></span>
+                                    echo $bienvenue;
+
+                                }  ?> </span>
                                 <img class="img-profile rounded-circle"
                                     src="img/undraw_profile.svg">
                             </a>
                             <!-- Dropdown - User Information -->
-                            <!--div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
+                            <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
                                 aria-labelledby="userDropdown">
                                 <a class="dropdown-item" href="#">
                                     <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
@@ -243,7 +244,7 @@ $_SESSION["bin"];
                                     <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
                                     Logout
                                 </a>
-                            </div-->
+                            </div>
                         </li>
 
                     </ul>
@@ -255,58 +256,21 @@ $_SESSION["bin"];
                 <div class="container-fluid">
 
                     <!-- Page Heading -->
-                    <h1 class="h3 mb-2 text-gray-800">Affichage des administrateurs</h1>
+                    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+                        <h1 class="h3 mb-0 text-gray-800">Accueil</h1>
+                        <a href="deconnexion.php" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
+                                class="fas fa-download fa-sm text-white-50"></i> Se déconnecter</a>
+                    </div>
 
-                    <!-- DataTales Example -->
-                    <div class="card shadow mb-4">
-                        <div class="card-body">
-                            <div class="table-responsive">
-                                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-                                    <thead>
-                                        <tr>
-                                            <th>id</th>
-                                            <th>Nom d'utilisateur</th>
-                                            <th>Nom</th>
-                                            <th>Prénom</th>
-                                            <th>Accréditation</th>
-                                            <th>Options</th>
-                                        </tr>
-                                    </thead>
-                                    <!--tfoot>
-                                        <tr>
-                                            <th>id</th>
-                                            <th>Nom d'utilisateur</th>
-                                            <th>Nom</th>
-                                            <th>Prénom</th>
-                                            <th>Accréditation</th>
-                                            <th>Salary</th>
-                                        </tr>
-                                    </tfoot-->
-                                    <?php
-                                        include("Connexion.php");
-                                        //include("ProjetRecuperation.php");
-                                        $requet=$bdd->query('select id,nom_ut,Nom,Prenom,Accreditation from administrateur');
-                                        $requet->execute();
-                                        while($data= $requet->fetch())
-                                            {   
+                    <!-- Content Row -->
+                    <div class="row">
 
-            	                              echo "<tbody>";
-                                              echo "<tr>";
-                                              $id=$data['id'];
-                echo" <td>".$data['id']."</td>
-                    <td>".$data['nom_ut']."</td>
-                    <td>".$data['Nom']."</td>
-                    <td>".$data['Prenom']."</td>
-                    <td>".$data['Accreditation']."</td>
-                    <td><button type='submit'><a href='SupprimerAdmin.php?id=$id'> Supprimer</a></button></td>";
+                        
 
-                    echo"</tr>";
-                echo "<tbody>";
-            }
-            ?> 
-                                    
-                                </table>
-                            </div>
+                        <!-- Pie Chart -->
+                        <div class="col-xl-4 col-lg-5">
+                            
+
                         </div>
                     </div>
 
@@ -320,7 +284,7 @@ $_SESSION["bin"];
             <footer class="sticky-footer bg-white">
                 <div class="container my-auto">
                     <div class="copyright text-center my-auto">
-                        <span>Copyright &copy; Orabank Togo 2022</span>
+                        <span>Copyright &copy; Your Website 2021</span>
                     </div>
                 </div>
             </footer>
@@ -333,12 +297,12 @@ $_SESSION["bin"];
     <!-- End of Page Wrapper -->
 
     <!-- Scroll to Top Button-->
-    <!--a class="scroll-to-top rounded" href="#page-top">
+    <a class="scroll-to-top rounded" href="#page-top">
         <i class="fas fa-angle-up"></i>
-    </a-->
+    </a>
 
     <!-- Logout Modal-->
-    <!--div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+    <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
         aria-hidden="true">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
@@ -355,7 +319,7 @@ $_SESSION["bin"];
                 </div>
             </div>
         </div>
-    </div-->
+    </div>
 
     <!-- Bootstrap core JavaScript-->
     <script src="vendor/jquery/jquery.min.js"></script>
@@ -368,11 +332,11 @@ $_SESSION["bin"];
     <script src="js/sb-admin-2.min.js"></script>
 
     <!-- Page level plugins -->
-    <script src="vendor/datatables/jquery.dataTables.min.js"></script>
-    <script src="vendor/datatables/dataTables.bootstrap4.min.js"></script>
+    <script src="vendor/chart.js/Chart.min.js"></script>
 
     <!-- Page level custom scripts -->
-    <script src="js/demo/datatables-demo.js"></script>
+    <script src="js/demo/chart-area-demo.js"></script>
+    <script src="js/demo/chart-pie-demo.js"></script>
 
 </body>
 
--- recup.php
+++ recup.php
@@ -1,9 +1,9 @@
 <?php
 session_start();
 include("Connexion.php");
+//include("SuperAdmin.php");
 $erreur= null;
 if (isset($_POST['nom_ut']) && isset($_POST['password']) && isset($_POST['Accreditation'])) {
-	
 	$bar=sha1($_POST['password']);
 	$insertion=$bdd->prepare("select * from administrateur where nom_ut=? and password=? and Accreditation=?");
     $insertion->execute(array($_POST['nom_ut'],$bar,$_POST['Accreditation']));
@@ -21,7 +21,7 @@ header("location:SuperAdmin.php");
 		$_SESSION["bin"] = ucfirst(strtoupper($user[0]["Nom"])) .
 " "  .  strtolower($user[0]["Prenom"]);
 $_SESSION["connecter"] = "yes";
-header("location:session_admin.php");
+header("location:SecondAdmin.php");
 	}else{
 		echo "Erreur !";
 	}
